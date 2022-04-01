471e33f542b02db47f374c031ab0614cfd2ef255
Aindrail <aindrailsantra@gmail.com>
2022-04-02 03:12:50 +0530

    useEffect edited

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,12 @@ export default function Home() {
     })
 
 
-  // const map = new mapboxgl.Map({
-  //   container: 'map', 
-  //   style: 'mapbox://styles/mapbox/streets-v11',
-  //   center: [-74.5, 40], 
-  //   zoom: 9 
-  //   });
+  const map = new mapboxgl.Map({
+    container: 'map', 
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-74.5, 40], 
+    zoom: 9 
+    });
 
   return (
     <Wrapper>
