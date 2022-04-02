362cbb4024c31912445d8e8b3c5cc4c3f02c4c66
Aindrail <aindrailsantra@gmail.com>
2022-04-03 01:39:15 +0530

    Input container created

--- pages/search.js
+++ pages/search.js
@@ -11,14 +11,14 @@ const Search = () => {
       </ButtonContainer>
 
       <InputContainer>
-       {/* <FromToIcons>
+       <FromToIcons>
          <Circle src="https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png" />
 
          <Line src = "https://img.icons8.com/ios/50/9CA3AF/vertical-line.png"  />
 
          <Square src = "https://img.icons8.com/windows/50/000000/square-full.png" />
 
-       </FromToIcons> */}
+       </FromToIcons>
 
        <InputBoxes>
        
