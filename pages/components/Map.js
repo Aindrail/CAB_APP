66c91aeea3be82539c70930a6962e0e7e2e43350
Aindrail <aindrailsantra@gmail.com>
2022-04-02 11:40:16 +0530

    Map.js file created in component folder and edited

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import tw from "tailwind-styled-components"
  import mapboxgl from '!mapbox-gl';
- import { useEffect } from 'react';
+ import { useEffect } from "react";
 
 
 
- mapboxgl.accessToken = 'pk.eyJ1IjoiYWluZHJhaWwiLCJhIjoiY2wxZ3gxdXRmMTdnZjNvbG50Y25nZTZ0ciJ9.GNxD2xX5SOb_YC63s9Hiaw';
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYWluZHJhaWwiLCJhIjoiY2wxZ3gxdXRmMTdnZjNvbG50Y25nZTZ0ciJ9.GNxD2xX5SOb_YC63s9Hiaw';
+// mapboxgl.accessToken = 'pk.eyJ1IjoibmF6YXJpeTE5OTUiLCJhIjoiY2t2bGlmdW12MHZlcDJ1bzA5OHh3NDIxeCJ9.li8l-1u52aCFd2ZdW-1IaA';
 
 const Map = () => {
 
@@ -16,7 +17,7 @@ const Map = () => {
         style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
         center: [77.947998, 23.473324],
         zoom: 5
-        })
+        });
         })
 
   return (
