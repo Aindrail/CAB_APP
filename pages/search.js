5f226b0ab8e9468bdb0af09921dcf768d64d8cff
Aindrail <aindrailsantra@gmail.com>
2022-04-03 01:37:42 +0530

    Imput container created

--- pages/search.js
+++ pages/search.js
@@ -10,7 +10,7 @@ const Search = () => {
           <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" /> 
       </ButtonContainer>
 
-      <InputContainer>
+      {/* <InputContainer>
        <FromToIcons>
          <Circle src="https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png" />
 
@@ -27,7 +27,7 @@ const Search = () => {
        
        </InputBoxes>
       <PlusIcon src="https://img.icons8.com/ios/50/000000/plus-math.png" />
-      </InputContainer>
+      </InputContainer> */}
 
       <SavedPlaces>
 
