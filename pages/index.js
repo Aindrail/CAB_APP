cbc240bc791425b908653e939abdb5a9276009eb
Aindrail <aindrailsantra@gmail.com>
2022-04-03 00:53:10 +0530

    Main page edited and styled

--- pages/index.js
+++ pages/index.js
@@ -97,6 +97,6 @@ const ActionButtonImage = tw.img `
 h-3/5
 `
 
-const InputButtons = tw.div `
-h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8
-`
+// const InputButtons = tw.div `
+// h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8
+// `
