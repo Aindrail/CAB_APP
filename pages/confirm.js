757d7b6440ed88b61ab3a9a772b0d74e383da366
Aindrail <aindrailsantra@gmail.com>
2022-04-04 01:24:18 +0530

    Pickup and dropdown coordinates function added

--- pages/confirm.js
+++ pages/confirm.js
@@ -14,12 +14,12 @@ const Confirm = () => {
      const pickup = "Bidanasi";
      //Fetch() calls API
 
-     fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?`+ // paste url with acces token
-       new URLSearchParams({
-           access_token: "pk.eyJ1IjoiYWluZHJhaWwiLCJhIjoiY2wxZ3gxdXRmMTdnZjNvbG50Y25nZTZ0ciJ9.GNxD2xX5SOb_YC63s9Hiaw",
-           limit:1 //only one thing from the data
-       })
-     )
+    //  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickup}.json?`+ // paste url with acces token
+    //    new URLSearchParams({
+    //        access_token: "pk.eyJ1IjoiYWluZHJhaWwiLCJhIjoiY2wxZ3gxdXRmMTdnZjNvbG50Y25nZTZ0ciJ9.GNxD2xX5SOb_YC63s9Hiaw",
+    //        limit:1 //only one thing from the data
+    //    })
+    //  )
      .then(response => response.json())
      .then(data =>{
         //  console.log(data.features[0].center)
