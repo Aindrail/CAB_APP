cc50f920ddbd81644429e28ba6ed58f085545fea
Aindrail <aindrailsantra@gmail.com>
2022-04-04 01:26:02 +0530

    Fetch fun is edited according to need

--- pages/confirm.js
+++ pages/confirm.js
@@ -29,14 +29,14 @@ const Confirm = () => {
 
   const getDropofCoordinates = () => {
     const dropof = "Bhubhaneswar";
-    //Fetch() calls API
+    Fetch() calls API
 
-    // fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropof}.json?`+ // paste url with acces token
-    //   new URLSearchParams({
-    //       access_token: "pk.eyJ1IjoiYWluZHJhaWwiLCJhIjoiY2wxZ3gxdXRmMTdnZjNvbG50Y25nZTZ0ciJ9.GNxD2xX5SOb_YC63s9Hiaw",
-    //       limit:1 //only one thing from the data
-    //   })
-    // )
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropof}.json?`+ // paste url with acces token
+      new URLSearchParams({
+          access_token: "pk.eyJ1IjoiYWluZHJhaWwiLCJhIjoiY2wxZ3gxdXRmMTdnZjNvbG50Y25nZTZ0ciJ9.GNxD2xX5SOb_YC63s9Hiaw",
+          limit:1 //only one thing from the data
+      })
+    )
     .then(response => response.json())
     .then(data =>{
         // console.log(data.features[0].center)
